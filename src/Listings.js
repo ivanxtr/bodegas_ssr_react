348eacc9bd0b22b5622cf90b4f4a894122bd8824
Jorge Rodriguez <jorge.rodriguez@SAO-MC-201007D.local>
2021-01-06 20:46:54 -0600

    adding UI enhace

--- src/Listings.js
+++ src/Listings.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { ApiCallsFactory } from './factories/apiCalls'
 
 function Listings() {
 
+    const [listings, setListings] = useState([])
     useEffect(() => {
         async function fetchData() {
             const call = new ApiCallsFactory('/api/listings')
             const response = await call.getData()
-            console.log(response)
+            setListings(response)
         }
         fetchData()
     },[])
@@ -32,39 +33,43 @@ function Listings() {
             <div className="untree_co-section bg-light">
                 <div className="container">
                     <div className="row">
-                        
-                        <div className="col-md-6 col-lg-4">
-                            <div className="property-entry">
-                                <img src="https://preview.colorlib.com/theme/homey/images/property_1.jpg" alt="Image" className="img-fluid"/>
-                                <div className="property-specs">
-                                <strong className="price d-inline-block">$1,900</strong>/month
-                                    <ul className="list-unstyled specs">
-                                    <li className="d-inline-flex align-items-center">
-                                        <span className="icon-wrap flaticon-bathtub"></span>
-                                        <strong>2</strong>
-                                    </li>
-                                    <li className="d-inline-flex align-items-center">
-                                        <span className="icon-wrap flaticon-bed"></span>
-                                        <strong>4</strong>
-                                    </li>
-                                    <li className="d-inline-flex align-items-center">
-                                        <span className="icon-wrap flaticon-house-size"></span>
-                                        <strong>120<sup>2</sup></strong>
-                                    </li>
-                                    </ul>
-                                    <div className="location d-flex justify-content-between align-items-center">
-                                    <div>
-                                        <span className="d-block caption">location: </span>
-                                        <h3><span>2 Zwar Place, Florey</span></h3>
+                        { 
+                            listings.map((item, index) => {
+                                return (
+                                    <div className="col-md-6 col-lg-4" key={index}>
+                                        <div className="property-entry">
+                                            <img src={'http://localhost:8000' + item.photo_1} alt="Image" className="img-fluid"/>
+                                            <div className="property-specs">
+                                            <strong className="price d-inline-block">$1,900</strong>/month
+                                                <ul className="list-unstyled specs">
+                                                <li className="d-inline-flex align-items-center">
+                                                    <span className="icon-wrap flaticon-bathtub"></span>
+                                                    <strong>2</strong>
+                                                </li>
+                                                <li className="d-inline-flex align-items-center">
+                                                    <span className="icon-wrap flaticon-bed"></span>
+                                                    <strong>4</strong>
+                                                </li>
+                                                <li className="d-inline-flex align-items-center">
+                                                    <span className="icon-wrap flaticon-house-size"></span>
+                                                    <strong>120<sup>2</sup></strong>
+                                                </li>
+                                                </ul>
+                                                <div className="location d-flex justify-content-between align-items-center">
+                                                <div>
+                                                    <span className="d-block caption">location: </span>
+                                                    <h3><span>2 Zwar Place, Florey</span></h3>
+                                                </div>
+                                                <div className="more">
+                                                    <span className="icon-keyboard_backspace"></span>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div> 
                                     </div>
-                                    <div className="more">
-                                        <span className="icon-keyboard_backspace"></span>
-                                    </div>
-                                    </div>
-                                </div>
-                            </div> 
-                        </div>
-
+                                )
+                            })
+                        }
                     </div>
                 </div>
                 <div className="row mt-5">

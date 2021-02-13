41e17bbad6b9a4146d575b0e4d7d97c64c64002b
Ivan Rodriguez <sbr.com@hotmail.com>
2021-02-13 14:11:56 -0600

    Add gzip

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+import compression from 'compression';
 
 import React from "react";
 import ReactDOMServer from "react-dom/server";
@@ -14,6 +15,9 @@ import Locale from '../src/Listing'
 const PORT = process.env.PORT || 8001;
 
 const app = express();
+
+// Compress all HTTP responses
+app.use(compression());
 
 app.get("/", (req, res, next) => {
   fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
